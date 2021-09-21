344534aa4e61432238192a929ca2ff8e9fe17115
aylivdele <irassomahin@gmail.com>
2021-09-21 19:00:49 +0700

    Removed extra code

--- src/main/java/com/ilya/scooters/ScootersApplication.java
+++ src/main/java/com/ilya/scooters/ScootersApplication.java
@@ -18,35 +18,4 @@ public class ScootersApplication {
         SpringApplication.run(ScootersApplication.class, args);
     }
 
-    @Autowired
-    RideRepos rideRepos;
-
-    @Autowired
-    ScooterRepos scooterRepos;
-
-    @Autowired
-    UserRepos userRepos;
-
-
-    public String addSomeObjects(){
-//        User ilya = new User("Ilya");
-//        User sasha = new User("Sasha");
-//        userRepos.save(ilya);
-//        userRepos.save(sasha);
-//
-//        Scooter scooter1 = new Scooter("SuperScooter3000", 100);
-//        Scooter scooter2 = new Scooter("Lightning", 95);
-//
-//        scooterRepos.save(scooter1);
-//        scooterRepos.save(scooter2);
-//
-//        Ride ride1 = new Ride(999999,123457.12, 654322.45, 123456.12, 654321.43, 6666,1234, scooter1, sasha);
-//        Ride ride2 = new Ride(123,111111.11, 222222.22, 555555.55, 666666.66, 6666,777, scooter2, ilya);
-//
-//        rideRepos.save(ride1);
-//        rideRepos.save(ride2);
-//
-        return "";
-    }
-
 }
